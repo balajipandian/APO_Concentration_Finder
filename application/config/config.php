6d612989513a9ddb4d8b562ae6df1ba3b34937d3
Arthur Barrett <abarrett@edx.org>
2014-04-03 14:20:20 -0400

    modify for huit hosting

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.balajipandian.com/test/huit2';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
